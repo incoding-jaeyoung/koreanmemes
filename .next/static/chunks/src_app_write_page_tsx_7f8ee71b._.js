28f306300bebcdeafafef70f118f91e49d721c90
조재영 <contact@incoding.co.kr>
2025-06-08 16:11:06 +0900

    fix: .next 폴더 제거

--- .next/static/chunks/src_app_write_page_tsx_7f8ee71b._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_ImageTranslator_tsx_e60c2a5f._.js",
-  "static/chunks/src_dc5992f7._.js",
-  "static/chunks/node_modules_498b93fe._.js"
-],
-    source: "dynamic"
-});
